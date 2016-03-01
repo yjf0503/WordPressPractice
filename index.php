9244eac44de30143d05ebaa952baa0621d833810
jiefuyang <a1641575@student.adelaide.edu.au>
2016-03-01 23:25:07 +1100

    Add the metadata of posts to the index page

--- index.php
+++ index.php
@@ -54,7 +54,19 @@
 <div id="container">
  <?php if(have_posts()): ?><?php while(have_posts()): the_post(); ?>
 
-  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+     <div class="post">
+         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+        <div class="enrty">
+         <?php the_content(); ?>
+
+         <p class="postmetadata">
+          <?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
+          <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
+         </p>
+        </div>
+
+     </div>
 
  <?php endwhile; ?>
 
